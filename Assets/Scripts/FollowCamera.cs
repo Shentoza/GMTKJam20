1bc8a0bb93c36b3dd91118dc8a33f9b21cec7feb
Shentoza <wuelk@hotmail.de>
2020-07-10 22:47:00 +0200

    SAve

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject FollowingGameObject;
+
+    private Rigidbody2D FollowRigid;
+
+    private Vector2 Velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Velocity = Vector2.zero;
+        if (FollowingGameObject)
+        {
+            FollowRigid = FollowingGameObject.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y) +
+                             (FollowRigid.velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/GameObjects/Rocket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody2D RigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RigidBody = GetComponent<Rigidbody2D>();
+        RigidBody.velocity = new Vector2(0.0f, 10.0f);
+    }
+
+    private void Launch()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            RigidBody.gravityScale = 1.0f;
+        }
+    }
+}
